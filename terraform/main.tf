ef9e34ed58e281765b7790f016a193f3f775c46b
Oleh-Hudzo <myhudzio@gmail.com>
2023-03-04 23:59:54 +0200

    refactor: move VPC to separate module

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,12 +11,3 @@ provider "aws" {
   profile = var.profile
   region  = var.region
 }
-
-# VPC
-resource "aws_vpc" "wp-vpc" {
-  cidr_block       = "10.0.0.0/16"
-
-  tags = {
-    Name = "${var.default_tag}-vpc"
-  }
-}
